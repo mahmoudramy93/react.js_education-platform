c91f3d2aef35d7fb6dfa0f9d54a8e92cca65c1cd
Safeya-Yasien <bluesky7yas@gmail.com>
2024-07-16 23:22:30 +0300

    update course form

---
+++ src/components/AdminDashboard/UpdateCourse/UpdateCourse.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateCourse,
+  fetchCourseById,
+} from "../../../rtk/features/courses/coursesActions";
+import SuccessMessage from "../../SuccessMessage/SuccessMessage";
+import { resetSuccess } from "../../../rtk/features/courses/courseSlice";
+import { useNavigate, useParams } from "react-router-dom";
+
+const UpdateCourse = () => {
+  const dispatch = useDispatch();
+  const courseData = useSelector((state) => state.courses.courses);
+  const success = useSelector((state) => state.courses.success);
+  const navigate = useNavigate();
+  const { courseId } = useParams();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [track, setTrack] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchCourseById(courseId));
+  }, [dispatch, courseId]);
+
+  useEffect(() => {
+    if (courseData) {
+      setTitle(courseData.title || "");
+      setDescription(courseData.description || "");
+    }
+  }, [courseData]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newData = {
+      title: title,
+      description: description,
+    };
+
+    dispatch(updateCourse({ courseId, data: newData }));
+  };
+
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => {
+        dispatch(resetSuccess());
+        navigate(-1);
+      }, 1000);
+    }
+  }, [success]);
+
+  return (
+    <div className="addCourse">
+      <p>Update Course</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="course-name">Course Name</label>
+        <input
+          type="text"
+          id="course-name"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="course-description">Course Description:</label>
+        <input
+          type="text"
+          id="course-description"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <label htmlFor="course-track">Course Track:</label>
+        <input
+          type="text"
+          id="course-track"
+          name="track"
+          value={track}
+          onChange={(e) => setTrack(e.target.value)}
+        />
+
+        <button type="button" className="btn btn-secondary mb-4">
+          Update Lesson
+        </button>
+        <button type="submit" className="btn btn-success">
+          Update Course
+        </button>
+      </form>
+      {success ? (
+        <SuccessMessage message={"Data Updated Successfully"} />
+      ) : null}
+    </div>
+  );
+};
+
+export default UpdateCourse;
