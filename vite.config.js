b082fc8c6e38124cf785e02e97ba15f92e1f9b93
Safeya-Yasien <bluesky7yas@gmail.com>
2024-07-16 22:54:43 +0300

    main configuration files

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePluginRequire from "vite-plugin-require";
+
+export default defineConfig({
+  plugins: [react(), vitePluginRequire.default()],
+  server: {
+    port: 5000,
+  },
+  base: "/edu_city/",
+});
